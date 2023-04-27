77f04c333f7cb09ba60ecee5b3e8dee67de7cdbe
Otshepeng Setuke <shaniquah.setuke@gmail.com>
2023-04-27 17:59:06 +0200

    modified code

--- challenge-1/script.js
+++ challenge-1/script.js
@@ -3,12 +3,8 @@ const secondaryPhone = '0219131568'
 
 // Only change below this line
 
-// const primaryValid = typeof primaryPhone === 'number'
-// const secondaryValid = typeof secondaryPhone == 'number'
-
-const primaryValid = typeof  primaryPhone == 'number'
-const secondaryValid = typeof secondaryPhone === 'string'
+const primaryValid = typeof  parseInt(primaryPhone) === 'number'    /* console should return NaN because the first character of the string is a non-numeric character */
+const secondaryValid = typeof parseInt(secondaryPhone) === 'number' /* console can only parseInt() a numerical string */
 
 console.log('Primary phone is valid numerical string:', primaryValid);
 console.log('Secondary phone is valid numerical string:', secondaryValid);
- 
---
+++ challenge-2/script.js
@@ -0,0 +1,31 @@
+const rent = 400;
+const tax = '8%';
+const food = 51.7501;
+const salary = 800;
+const transport = 10.2;
+const hourOfDay = 00;
+const minuteOfDay = 00;
+
+// Only change below this line
+
+if ((hourOfDay !== null && minuteOfDay !== null) && (hourOfDay === 00 && minuteOfDay === 00)) {
+	const taxAsDecimal = parseInt(tax) / 100; 
+    const startingAfterTax = salary * 1 - taxAsDecimal;
+	const balance = startingAfterTax - transport - food - rent;
+    console.log(balance.toFixed(2));
+}    
+
+
+/* BUG FIXES:
+ * 
+ * fixed if statement format, 
+ * modified conditions, 
+ * coerced tax to numeric value, 
+ * rewrote '100' as number because strings can't divide, 
+ * rewrote '1' as numeric value for valid multiplication, 
+ * fixed typo in 'balance'('balace' was not going to fetch undeclared variable), 
+ * moved 'console.log' inside block-scope to access 'balance' variable, 
+ * modified '.toFixed(3)' to '.toFixed(2)' 
+ * code only runs and calculates balance once all conditions in if boolean are met
+ * 
+*/
