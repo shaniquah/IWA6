2a9f2cf5fb8de660ac1aa157b4dcf4db3873a425
Otshepeng Setuke <shaniquah.setuke@gmail.com>
2023-04-29 15:49:43 +0200

    Update script.js

--- challenge-1/script.js
+++ challenge-1/script.js
@@ -3,7 +3,7 @@ const secondaryPhone = '0219131568'
 
 // Only change below this line
 
-const primaryValid = typeof  parseInt(primaryPhone) === 'number'    /* console should return NaN because the first character of the string is a non-numeric character */
+const primaryValid = parseInt(primaryPhone) === 'number'    /* console should return NaN because the first character of the string is a non-numeric character */
 const secondaryValid = typeof parseInt(secondaryPhone) === 'number' /* console can only parseInt() a numerical string */
 
 console.log('Primary phone is valid numerical string:', primaryValid);
